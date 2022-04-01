84000cb6a933e7060b7809c1262f98cd2600cf2e
Abhishek Jha <abhi170599@gmail.com>
2022-04-01 16:12:00 +0530

    Add hateos links

--- src/main/java/com/absoft/springsample/controllers/PostController.java
+++ src/main/java/com/absoft/springsample/controllers/PostController.java
@@ -1,17 +1,25 @@
 package com.absoft.springsample.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import com.absoft.springsample.entities.Post;
 import com.absoft.springsample.exceptions.NotFoundException;
 import com.absoft.springsample.repositories.PostRepository;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class PostController {
@@ -25,17 +33,30 @@ public class PostController {
     }
 
     @GetMapping(path = "/posts/{id}")
-    public Post retrievePost(@PathVariable int id) {
+    public EntityModel<? extends Object> retrievePost(@PathVariable int id) {
         Post post = postRepo.findOne(id);
         if (post == null) {
             throw new NotFoundException(String.format("Post not found id = %s", id));
         }
-        return post;
+        EntityModel<Post> resource = EntityModel.of(post);
+        WebMvcLinkBuilder linkToAll = linkTo(methodOn(this.getClass()).retrieveAllPosts());
+        WebMvcLinkBuilder linkToUserPosts = linkTo(methodOn(this.getClass()).retrieveUserPosts(post.getId()));
+
+        resource.add(linkToAll.withRel("all-posts"));
+        resource.add(linkToUserPosts.withRel("user-posts"));
+
+        return resource;
     }
 
     @PostMapping(path = "/posts")
-    public Post createPost(@RequestBody Post post) {
-        return postRepo.save(post);
+    public ResponseEntity<Object> createPost(@RequestBody Post post) {
+        Post savedPost = postRepo.save(post);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedPost.getId()).toUri();
+
+        return ResponseEntity.created(location).build();
     }
 
     @GetMapping(path = "/posts/user/{userId}")
